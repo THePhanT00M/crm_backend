2ad3290dd7b0888ffd8c91804d8ed29ca192b054
yuminji <yuminji97@kakao.com>
2024-06-03 15:22:35 +0900

    init

---
+++ src/test/java/site/shcrm/shcrm_backend/ShcrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shcrm.shcrm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShcrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
