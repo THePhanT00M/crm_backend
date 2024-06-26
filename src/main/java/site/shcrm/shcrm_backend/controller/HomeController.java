de2a5aafd70a39527cfa5c5c7cb8fcf2e09e8fc5
wpcks99 <wpcks99@naver.com>
2024-06-26 21:40:11 +0900

    6.26 mysql 연동 mvc 컨트롤러 추가

---
+++ src/main/java/site/shcrm/shcrm_backend/controller/HomeController.java
@@ -0,0 +1,14 @@
+package site.shcrm.shcrm_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
