258fd87c2930b85571b221b38f65d21a79efc067
wpcks99 <120312941+wpcks99@users.noreply.github.com>
2024-06-30 02:09:52 +0900

    Add files via upload

---
+++ src/main/java/site/shcrm/shcrm_backend/DTO/JoinDTO.java
@@ -0,0 +1,12 @@
+package site.shcrm.shcrm_backend.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class JoinDTO {
+    
+    private String username;
+    private String password;
+}
